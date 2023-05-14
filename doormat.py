a81ae13f65639f42fb1bc645b5d9ef887ad9e5d5
sritish <sritishkumargouda@gmail.com>
2023-05-14 16:32:37 +0530

    version2

--- doormat.py
+++ doormat.py
@@ -1,4 +1,6 @@
-l,s=list(map(int,input().split()))
+# ---------------- version 1
+
+'''l,s=list(map(int,input().split()))
 r=2
 t=int((l+1)/2)
 for i in range(1,l+1):
@@ -8,7 +10,20 @@ for i in range(1,l+1):
         print('WELCOME'.center(s,'-'))
     elif i>(t):
         print(((i-r)*'.|.').center(s,'-'))
+        r+=2'''
+
+# ----------------- version 2
+
+l,s=list(map(int,input().split()))
+r=2
+t=int((l+1)/2)
+o=[i for i in range(l) if i%2==1 ]
+
+for i in range(0,l):
+    if i<(t-1):
+        print((o[i]*'.|.').center(s,'-'))
+    elif i==(t-1):
+        print('WELCOME'.center(s,'-'))
+    elif i>(t-1):
+        print(((o[i-r])*'.|.').center(s,'-'))
         r+=2
-
-
-    
