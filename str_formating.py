130c2f86081738e121438d7be53cfe5a9d22a48c
sritish <sritishkumargouda@gmail.com>
2023-05-19 19:15:28 +0530

    my version

--- str_formating.py
+++ str_formating.py
@@ -1,4 +1,4 @@
-def print_formatted(number):
+'''def print_formatted(number):
     pad = number.bit_length()
     for i in range(1,number+1):
         print(str(i).rjust(pad),oct(i).split("o")[1].rjust(pad),hex(i).split("x")[1].upper().rjust(pad),bin(i).split("b")[1].rjust(pad))
@@ -6,4 +6,11 @@ def print_formatted(number):
         
 if __name__ == '__main__':
     n = int(input())
-    print_formatted(n)
+    print_formatted(n)'''
+
+# ------------------     my version
+n=int(input())
+bit=len(bin(n)[2:])
+
+for i in range(1,n+1):
+    print(str(i).rjust(bit,' '),oct(i)[2:].rjust(bit,' '),hex(i).upper()[2:].rjust(bit,' '),bin(i)[2:].rjust(bit,' '))
